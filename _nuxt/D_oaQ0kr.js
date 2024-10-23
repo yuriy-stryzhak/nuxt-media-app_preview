a6f112d590559fb1b57329df27047931abbe980f
yuriy-stryzhak <60847477+yuriy-stryzhak@users.noreply.github.com>
2024-10-23 17:10:52 +0300

    Add files via upload

---
+++ _nuxt/D_oaQ0kr.js
@@ -0,0 +1,1 @@
+import{u as e}from"./BwC3k-Vi.js";import{a8 as o,m as a}from"./nDQ2BZ9T.js";const u=o((t,r)=>{if(!e().isAuthenticated&&t.path==="/favorites")return a("/signin")});export{u as default};
